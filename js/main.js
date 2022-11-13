86e8e7a2cdbac12d91474d24cbc76aa53ce85f04
Mackenzie Lee <lee.n.mackenzie@gmail.com>
2022-11-12 17:07:42 -0800

    add route for all

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+async function apiGreetingCall(){
+    try{
+        const response = await fetch(`https://korean-greetings-api.herokuapp.com/api/all`)
+        const data = await response,json()
+
+        console.log(data)
+    } catch (err) {
+        console.log(err)
+    }
+
+}
