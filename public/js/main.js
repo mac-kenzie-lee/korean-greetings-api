b3c5b18e8455c8b73f01f4d6a5395270795b59ef
Mackenzie Lee <lee.n.mackenzie@gmail.com>
2022-11-12 17:49:20 -0800

    added cors

--- public/js/main.js
+++ public/js/main.js
@@ -3,9 +3,9 @@
      try{
          const response = await fetch(`https://korean-greetings-api.herokuapp.com/api/all`)
          const data = await response.json()
-
          console.log(data)
      } catch (err) {
+        console.log('not working')
          console.log(err)
      }
 }
