7004207709f84d720f2cb194425a1045f683db01
Mackenzie Lee <lee.n.mackenzie@gmail.com>
2022-11-12 18:32:01 -0800

    added styling and svg to index and css file

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,27 @@
+const greetingUl = document.querySelector("#placeForValidGreetings");
+
+
+
+
 
  async function apiGreetingCall(){
      try{
          const response = await fetch(`https://korean-greetings-api.herokuapp.com/api/all`)
          const data = await response.json()
-         console.log(data)
+         for (let i in data) {
+            if (i !== 'unknown')
+            createLi(i, greetingUl)
+         }
      } catch (err) {
-        console.log('not working')
+        console.log('not working, heroku not fetching: ')
          console.log(err)
      }
 }
 
 apiGreetingCall()
+
+function createLi(text, parent) {
+    let li = document.createElement("li");
+    li.textContent = text;
+    parent.appendChild(li)
+}
